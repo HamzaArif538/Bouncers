bf7ce8707a89b06363cb999a67f8fa612fbc0070
HamzaArif538 <sardarhamza538@gmail.com>
2024-02-14 00:25:13 +0500

    created the products page

--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    price = models.IntegerField(null=True)
+    oldprice = models.IntegerField(null=True)
+    color = models.CharField(max_length=50, null=True)
+    picture = models.ImageField(upload_to='product_images/', null=True, blank=True)
+    CATEGORY = (
+        ('Formal Shoes', 'Formal Shoes'),
+        ('Peshawari', 'Peshawari')
+    )
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    AVAILABILITY = (
+        ('In Stock', 'In Stock'),
+        ('Out of Stock', 'Out of Stock')
+    )
+    availability = models.CharField(max_length=200, null=True, choices=AVAILABILITY)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
@@ -7,7 +8,20 @@ def home(request):
     return render(request, 'core/home.html', context)
 
 def products(request):
-    context = {}
+    product = Product.objects.all()
+    totalproducts = Product.objects.count()
+
+    for item in product:
+        # Handle potential null values gracefully to avoid errors
+        if item.oldprice and item.price:
+            percentoff = round(((item.oldprice - item.price) / item.oldprice) * 100, 0)
+            
+        else:
+            percentoff = None  # Set to None if price or oldprice is missing
+
+        item.percentoff = percentoff
+    
+    context = {'product':product, 'totalproducts':totalproducts, 'percentoff':percentoff}
     return render(request, 'core/products.html', context)
 
 def aboutus(request):
